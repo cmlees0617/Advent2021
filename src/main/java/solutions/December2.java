659e4f7290c1a24d8540cb1eba6e801327f443ea
Caleb Lees <Caleb Lees@JOEL.woh.rr.com>
2021-12-03 23:07:48 -0500

    Added file I/O to December 2 (replaces System.in)

--- src/main/java/solutions/December2.java
+++ src/main/java/solutions/December2.java
@@ -1,34 +1,41 @@
 package solutions;
 
 import java.util.Scanner;
+import java.io.*;
 
 public class December2 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        try{
+            Scanner scan = new Scanner(
+                new File("December2_input.txt"));
+            
+            int hor = 0;
+            int depth = 0;
+            int aim = 0;
         
-        int hor = 0;
-        int depth = 0;
-        int aim = 0;
-        
-        while(scan.hasNext()) {
-            String curr = scan.next();
-            if (curr.equals("forward")) {
-                int i = scan.nextInt();
-                hor += i;
-                depth += aim * i;
-                System.out.println("forward: " + hor + " " + depth);
+            while(scan.hasNext()) {
+                String curr = scan.next();
+                if (curr.equals("forward")) {
+                    int i = scan.nextInt();
+                    hor += i;
+                    depth += aim * i;
+                    System.out.println("forward: " + hor + " " + depth);
+                }
+                else if (curr.equals("down")) {
+                    aim += scan.nextInt();
+                    System.out.println("aim: " + aim);
+                }
+                else if (curr.equals("up")) {
+                    aim -= scan.nextInt();
+                    System.out.println("aim: " + aim);
+                }
+                else {
+                    throw new RuntimeException("EXPLODED");
+                }
             }
-            else if (curr.equals("down")) {
-                aim += scan.nextInt();
-                System.out.println("aim: " + aim);
-            }
-            else if (curr.equals("up")) {
-                aim -= scan.nextInt();
-                System.out.println("aim: " + aim);
-            }
-            else {
-                throw new RuntimeException("EXPLODED");
-            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
         }
     }
 }
