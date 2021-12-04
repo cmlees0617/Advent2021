2bbb5cb7ab33fb5f69b5b27a742eb11883c30006
Caleb Lees <Caleb Lees@JOEL.woh.rr.com>
2021-12-03 23:01:22 -0500

    Added file I/O (replaces System.in)

--- src/main/java/solutions/December1.java
+++ src/main/java/solutions/December1.java
@@ -1,34 +1,39 @@
 package solutions;
 
 import java.util.Scanner;
-import java.io.File;
+import java.io.*;
 
 public class December1 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        try{
+            Scanner scan = new Scanner(
+                new File("C:\\Users\\Caleb Lees\\Documents\\NetBeansProjects\\Advent_Of_Code_2021\\December1_input.txt"));
+            Integer prevPrev = scan.nextInt();
+            Integer prev = scan.nextInt();
+            Integer curr = scan.nextInt();
         
-        Integer prevPrev = scan.nextInt();
-        Integer prev = scan.nextInt();
-        Integer curr = scan.nextInt();
+            int currSum = prevPrev + prev + curr;
+            int prevSum;
         
-        int currSum = prevPrev + prev + curr;
-        int prevSum;
+            int sumIncrease = 0;
         
-        int sumIncrease = 0;
-        
-        while (scan.hasNext()) {
-            prevPrev = prev;
-            prev = curr;
-            curr = scan.nextInt();
-            
-            prevSum = currSum;
-            currSum = prevPrev + prev + curr;
-            
-            if (currSum > prevSum) {
-                sumIncrease++;
-                System.out.println("increased " + sumIncrease);
+            while (scan.hasNext()) {
+                prevPrev = prev;
+                prev = curr;
+                curr = scan.nextInt();
+
+                prevSum = currSum;
+                currSum = prevPrev + prev + curr;
+
+                if (currSum > prevSum) {
+                    sumIncrease++;
+                }
             }
+            
+            System.out.println(sumIncrease);
         }
-        
+        catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 }
